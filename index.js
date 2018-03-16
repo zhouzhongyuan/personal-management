df9e0756c972a856d24830444afbed5498705f74
Zhongyuan <yawenzhongyuan@gmail.com>
2018-03-16 17:30:15 +0800

    Init

---
+++ index.js
@@ -0,0 +1,10 @@
+const wallpaper = require('wallpaper');
+
+// wallpaper.set('unicorn.jpg').then(() => {
+//     console.log('done');
+// });
+
+wallpaper.get().then(imagePath => {
+    console.log(imagePath);
+    //=> '/Users/sindresorhus/unicorn.jpg'
+});
